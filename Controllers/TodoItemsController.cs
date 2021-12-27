e4f7e7f63b5a1b9af8ebab895f49018130294027
GGolfz <wisarutgolf11@gmail.com>
2021-12-27 20:00:01 +0700

    add: update todo item :sparkles:

--- Controllers/TodoItemsController.cs
+++ Controllers/TodoItemsController.cs
@@ -13,22 +13,45 @@ public class TodoItemsController : ControllerBase
         _context = context;
     }
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems() {
+    public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+    {
         return await _context.TodoItems.ToListAsync();
     }
     [HttpGet("{id}")]
-    public async Task<ActionResult<TodoItem>> GetTodoItem(long id) {
+    public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+    {
         var todoItem = await _context.TodoItems.FindAsync(id);
-        if (todoItem == null) {
+        if (todoItem == null) return NotFound();
+        return todoItem;
+    }
+    [HttpPost]
+    public async Task<ActionResult<TodoItem>> CreateTodoItem(TodoItem todoItem)
+    {
+        _context.TodoItems.Add(todoItem);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
+    }
+    [HttpPut("{id}")]
+    public async Task<ActionResult<TodoItem>> UpdateTodoItem(long id, TodoItem todoItem)
+    {
+        if (id != todoItem.Id)
+        {
+            return BadRequest();
+        }
+        var item = await _context.TodoItems.FindAsync(id);
+        if (item == null) return NotFound();
+        item.Name = todoItem.Name;
+        item.Description = todoItem.Description;
+        item.IsComplete = todoItem.IsComplete;
+        try {
+            await _context.SaveChangesAsync();
+        } catch (DbUpdateConcurrencyException) when (!TodoItemExists(id)) {
             return NotFound();
         }
         return todoItem;
     }
-    [HttpPost]
-    public async Task<ActionResult<TodoItem>> CreateTodoItem(TodoItem todoItem) {
-        _context.TodoItems.Add(todoItem);
-        await _context.SaveChangesAsync();
-
-        return CreatedAtAction(nameof(GetTodoItem), new {id = todoItem.Id}, todoItem);
+    private bool TodoItemExists(long id) {
+        return _context.TodoItems.Any(e => e.Id.Equals(id));
     }
 }
