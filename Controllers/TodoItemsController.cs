17bd2c15e8ab42fafe2ea03d38033a74124a3138
GGolfz <wisarutgolf11@gmail.com>
2021-12-27 19:47:22 +0700

    add: create todo item :sparkles:

--- Controllers/TodoItemsController.cs
+++ Controllers/TodoItemsController.cs
@@ -24,4 +24,11 @@ public class TodoItemsController : ControllerBase
         }
         return todoItem;
     }
+    [HttpPost]
+    public async Task<ActionResult<TodoItem>> CreateTodoItem(TodoItem todoItem) {
+        _context.TodoItems.Add(todoItem);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetTodoItem), new {id = todoItem.Id}, todoItem);
+    }
 }
